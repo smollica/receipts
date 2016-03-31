99d453f80ed15075497d13c955e03a1b4964e151
smollica <snmollic@hotmail.com>
2016-03-31 11:39:42 -0700

    start

---
+++ Receipts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Receipts
+//
+//  Created by Monica Mollica on 2016-03-31.
+//  Copyright © 2016 Sergio Mollica. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
